c67f1cac4c589f96c3fb0b458b202ce5d8fad1dd
Sara Chein <sarahchein@gmail.com>
2024-07-07 16:54:21 -0400

    perf functionality

--- frontend/components/OrderList.js
+++ frontend/components/OrderList.js
@@ -1,42 +1,53 @@
-// import React from 'react'
-// import { useGetUsersQuery, } from '../state/store'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { setFilter } from '../state/store'
+// import React from 'react';
+// import { useGetPizzaHistoryQuery } from '../state/store';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { setFilter } from '../state/store';
 
 // export default function OrderList() {
-//   const { data: users, error, isLoading } = useGetUsersQuery();
-//   const dispatch = useDispatch()
-//   const filter = useSelector((state) => state.filter)
+//   const { data: orders } = useGetPizzaHistoryQuery();
+//   const dispatch = useDispatch();
+//   const filter = useSelector((state) => state.filter);
 
 //   const handleFilterChange = (newFilter) => {
-//     dispatch(setFilter(newFilter))
+//     dispatch(setFilter(newFilter));
 //   };
 
-//   const filteredUsers = users?.filter(user => filter === 'All' || user.size === filter)
+//   const filteredOrders = orders?.filter(order => filter === 'All' || order.size === filter);
 
-//   if (isLoading) return <div>Loading...</div>;
-//   if (error) return <div>Error loading users</div>
+//   // Commented out the loading and error states for testing purposes
+//   // if (isLoading) return <div>Loading...</div>;
+//   // if (error) return <div>Error loading orders</div>;
+//   console.log(orders);
 
 //   return (
 //     <div id="orderList">
 //       <h2>Pizza Orders</h2>
 //       <ol>
-//         {
-//          filteredOrders?.map(order => (
+//       {
+//           filteredOrders?.map(order => {
+//             const toppingsCount = order.toppings.length;
+//             let toppingsText = 'no toppings';
+//             if (toppingsCount === 1) {
+//               toppingsText = '1 topping';
+//             } else if (toppingsCount > 1) {
+//               toppingsText = `${toppingsCount} toppings`;
+//             }
+//             return (
 //               <li key={order.id}>
 //                 <div>
-//                   {order.fullName} - {order.size} - {order.toppings.join(',')}order details here
+//                   {`${order.customer} ordered a size ${order.size} with ${toppingsText}`}
 //                 </div>
 //               </li>
-//             ))
+//             );
+//           })
 //         }
 //       </ol>
 //       <div id="sizeFilters">
 //         Filter by size:
 //         {
 //           ['All', 'S', 'M', 'L'].map(size => {
-//             const isActive = size === filter
-//             const className = `button-filter${isActive ? 'active' : ''}`
+//             const isActive = size === filter;
+//             const className = `button-filter${isActive ? ' active' : ''}`;
 //             return <button
 //               data-testid={`filterBtn${size}`}
 //               className={className}
@@ -46,7 +57,7 @@
 //         }
 //       </div>
 //     </div>
-//   )
+//   );
 // }
 
 import React from 'react';
@@ -55,7 +66,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from '../state/store';
 
 export default function OrderList() {
-  const { data: orders } = useGetPizzaHistoryQuery();
+  const { data: orders, isLoading, error } = useGetPizzaHistoryQuery();
   const dispatch = useDispatch();
   const filter = useSelector((state) => state.filter);
 
@@ -65,18 +76,16 @@ export default function OrderList() {
 
   const filteredOrders = orders?.filter(order => filter === 'All' || order.size === filter);
 
-  // Commented out the loading and error states for testing purposes
-  // if (isLoading) return <div>Loading...</div>;
-  // if (error) return <div>Error loading orders</div>;
-  console.log(orders);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading orders</div>;
 
   return (
     <div id="orderList">
       <h2>Pizza Orders</h2>
       <ol>
-      {
+        {
           filteredOrders?.map(order => {
-            const toppingsCount = order.toppings.length;
+            const toppingsCount = order.toppings ? order.toppings.length : 0;
             let toppingsText = 'no toppings';
             if (toppingsCount === 1) {
               toppingsText = '1 topping';
@@ -110,5 +119,3 @@ export default function OrderList() {
     </div>
   );
 }
-
-
